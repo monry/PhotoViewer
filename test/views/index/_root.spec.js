9cd5f5dcf16fc1b5fbb35d434ce132c34bebd9d8
Tetsuya Mori <tetsuya.mori@kidsstar.co.jp>
2016-08-03 21:07:26 +0900

    write test: Root

---
+++ test/views/index/_root.spec.js
@@ -0,0 +1,26 @@
+import Root from 'js/views/index/_root';
+import Top from 'js/views/index/top';
+import Bottom from 'js/views/index/bottom';
+
+describe(
+  '<Root />',
+  () => {
+    it(
+      'has div#content',
+      (done) => {
+        const wrapper = shallow(<Root />);
+        expect(wrapper.find('div#content')).to.have.length(1);
+        done();
+      }
+    );
+
+    it(
+      'correct node',
+      (done) => {
+        const wrapper = shallow(<Root />);
+        expect(wrapper.contains(<div id="content"><Top /><Bottom /></div>)).to.equal(true);
+        done();
+      }
+    );
+  }
+);
