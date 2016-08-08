b5d8b95dc0ab6529e2171f332c343c0790eedd52
Tetsuya Mori <tetsuya.mori@kidsstar.co.jp>
2016-08-08 14:01:42 +0900

    load extension utilities

--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,8 @@
 require('file?name=../[name].[ext]!./html/index.html');
 require('./scss/index.scss');
 
+require('js/utilities/extensions.js');
+
 import Index from 'js/controllers/index';
 var index = new Index();
 index.renderView();
