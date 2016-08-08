4385a40742bb3dc8ab4987e84d7e3a29137fd014
Tetsuya Mori <tetsuya.mori@kidsstar.co.jp>
2016-08-08 16:42:07 +0900

    Do not import CSS into JS

--- src/entry.js
+++ src/entry.js
@@ -1,7 +1,5 @@
-// ファイルをコピー
+// 共通ライブラリ読み込み
 require('file?name=../[name].[ext]!./html/index.html');
-require('./scss/index.scss');
-
 require('js/utilities/extensions.js');
 
 import Index from 'js/controllers/index';
