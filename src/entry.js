b7a9f991e3d2be35db9bce9f6c49b603ab2eb0ae
Tetsuya Mori <tetsuya.mori@kidsstar.co.jp>
2016-08-10 12:43:39 +0900

    Load rx-jquery

--- src/entry.js
+++ src/entry.js
@@ -1,11 +1,14 @@
 // HTML ファイルコピー
 require('file?name=../[name].[ext]!./html/index.html');
 
-// 共通ライブラリ読み込み
+// 自作ライブラリ読み込み
 require('./js/utilities/extensions.js');
 
 // jQuery プラグイン読み込み
 require('jquery.scrollto');
+
+// その他ライブラリ読み込み
+require('rx-jquery');
 
 global.Const = {};
 window.$ = $;
