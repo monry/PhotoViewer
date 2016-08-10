95f4a1f1ac02a1a7415f924824f2d0b044b2750b
Tetsuya Mori <tetsuya.mori@kidsstar.co.jp>
2016-08-10 22:54:03 +0900

    Implement change photo method to controller class

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -29,23 +29,57 @@ export default class Index {
   }
 
   /**
+   * 次の写真を表示
+   */
+  next() {
+    if (this.selectionIndex.value + 1 >= Const.Image.count) {
+      return;
+    }
+    this.selectionIndex.value++;
+  }
+
+  /**
+   * 前の写真を表示
+   */
+  previous() {
+    if (this.selectionIndex.value <= 0) {
+      return;
+    }
+    this.selectionIndex.value--;
+  }
+
+  /**
+   * 指定の写真を表示
+   *
+   * @param {Number} index 写真番号
+   */
+  jump(index) {
+    if (0 <= index && index < Const.Image.count) {
+      this.selectionIndex.value = index;
+    }
+  }
+
+  /**
    * div#container ノードに定義されている設定を読み込む
    *
    * @private
    */
   _loadSetting() {
     const $container = $('#container');
-    Const.Image = {
-      count    : parseInt($container.attr('data-image-count')),
-      extension: $container.attr('data-image-extension'),
-      directory: {
-        photo    : $container.attr('data-image-directory-photo'),
-        thumbnail: $container.attr('data-image-directory-thumbnail'),
+    Const = {
+      Image: {
+        count    : parseInt($container.attr('data-image-count')),
+        extension: $container.attr('data-image-extension'),
+        directory: {
+          photo    : $container.attr('data-image-directory-photo'),
+          thumbnail: $container.attr('data-image-directory-thumbnail'),
+        },
+        aspectRatio: {
+          x: parseInt($container.attr('data-image-aspect-ratio-x')),
+          y: parseInt($container.attr('data-image-aspect-ratio-y')),
+        }
       },
-      aspectRatio: {
-        x: parseInt($container.attr('data-image-aspect-ratio-x')),
-        y: parseInt($container.attr('data-image-aspect-ratio-y')),
-      }
+      returnPath: $container.attr('data-return-path'),
     };
   }
 
